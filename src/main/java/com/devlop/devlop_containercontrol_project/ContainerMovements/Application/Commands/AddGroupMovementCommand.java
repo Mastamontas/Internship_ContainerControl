aed0d2f099662b721ace87b2dc46ecb3df5b47ff
Pedro Monteiro <pedro.monteiro@devlop.systems>
2024-12-02 14:23:10 +0000

    commit pipeline file

--- src/main/java/com/devlop/devlop_containercontrol_project/ContainerMovements/Application/Commands/AddGroupMovementCommand.java
+++ src/main/java/com/devlop/devlop_containercontrol_project/ContainerMovements/Application/Commands/AddGroupMovementCommand.java
@@ -1,9 +1,7 @@
 package com.devlop.devlop_containercontrol_project.ContainerMovements.Application.Commands;
-
+/**
+ * Painel igual movementHistory, mas com a opcao de selecionar varios containers
+ * e adicionar um movimento para metodos ao/aos os containers selecionados
+ */
 public class AddGroupMovementCommand {
-    //TODO: implementar
-    /**
-     * Painel igual movementHistory, mas com a opcao de selecionar varios containers
-     * e adicionar um movimento para metodos ao/aos os containers selecionados
-     */
 }
